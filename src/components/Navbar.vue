58f05c666b404d5ee565a0f593358ce8d5515270
Ameen <shehaaa4@gmail.com>
2025-11-21 20:55:31 +0200

    hkml bokra

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,14 @@
 <template>
   <section class="bg-[var(--primary)] text-[var(--white)] py-3">
-   <div class="containerr">asddsa</div>
+    <div class="containerr">
+      <div class="flex justify-between items-center">
+        <p class="font-medium text-xl">Home</p>
+        <h1 class="font-bold text-2xl">Quizify</h1>
+        <div class="flex justify-center items-center">
+          <div class="btn1">aloo</div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -8,4 +16,15 @@
 export default {};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.btn1 {
+  width: 40px;
+  height: 40px;
+  background-color: var(--secondary);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 12%;
+  padding: 20px 0;
+}
+</style>
